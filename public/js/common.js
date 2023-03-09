0060f88e510691d76b07e6acde7b1cfdadb06be4
ShinHyungJune <ssa4141@naver.com>
2023-03-09 14:30:13 +0900

    fix

--- public/js/common.js
+++ public/js/common.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".m-script-pop").click(function(){
         $($(this).attr("data-target")).toggle();
     });
+
+    $(".m-script-toggle").click(function(){
+        $($(this).attr("data-target")).toggleClass("active");
+    });
 });
